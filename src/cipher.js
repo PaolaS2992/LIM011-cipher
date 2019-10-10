82af075a65303d1a1a756a7138da8817007b552e
PaolaS2992 <huaripayta29.92@gmail.com>
2019-10-09 19:54:27 -0500

    LIM011-cipher-v4

--- src/cipher.js
+++ src/cipher.js
@@ -1,123 +1,101 @@
 window.cipher = {
   // Acá escribe tu código.
-  encode : (offset, string) => {
-
+  encode: (offset, string) => {
     let resultadoEncriptado = '';
+    for (let i = 0; i < string.length; i += 1) {
+      // ENGLOBAR "cada letra de la palabra string" EN UNA VARIABLE.
+      const palabra = string[i];
+      console.log(palabra);
 
-  for (let i = 0; i < string.length; i++) {  
+      // CONVERTIR "cada letra de la palabra string" EN CODIGO ASCII
+      const codigoAscci = string.charCodeAt(i);
+      console.log(codigoAscci);
 
-    // ENGLOBAR "cada letra de la palabra string" EN UNA VARIABLE.
-    let palabra = string[i];
-    console.log(palabra);
-
-    // CONVERTIR "cada letra de la palabra string" EN CODIGO ASCII
-    let codigoAscci = string.charCodeAt(i);
-    console.log(codigoAscci);
-
-    // ** VALIDANDO NUMERO POSITIVO **
-    if(offset > 0){
-      
+      // ** VALIDANDO NUMERO POSITIVO **
+      if (offset > 0) {
       // ** VALIDANDO LAS LETRAS MAYUSCULAS A = 65 y Z = 90 **
-      if(codigoAscci >= 65 && codigoAscci <= 90){
-
-        // FORMULA PARA ENCONTRAR LA NUEVA POSICIÓN EN EL CODIGO ASCCI.
-        let nuevaPosicion = ((codigoAscci - 65 + offset) % 26) + 65;
-        console.log(nuevaPosicion);
-    
-        // CONVERTIR LA NUEVA POSICIÓN DEL CÓDIGO ASCCI EN LETRA.
-        let stringEncriptado = String.fromCharCode(nuevaPosicion);
-        console.log(stringEncriptado);
-
-        //IMPRIMIR EN UNA VARIABLE EL ACUMULADO - LA PALABRA ENCRIPTADA.
-        resultadoEncriptado += stringEncriptado;
-      
-      // ** VALIDANDO LAS LETRAS MINUSCULAS A = 97 y Z = 122 **
-      }else if(codigoAscci >= 97 && codigoAscci <= 122){
-        
-        // FORMULA PARA ENCONTRAR LA NUEVA POSICIÓN EN EL CODIGO ASCCI.
-        let nuevaPosicion = ((codigoAscci - 97 + offset) % 26) + 97;
-        console.log(nuevaPosicion);
-    
-        // CONVERTIR LA NUEVA POSICIÓN DEL CÓDIGO ASCCI EN LETRA.
-        let stringEncriptado = String.fromCharCode(nuevaPosicion);
-        console.log(stringEncriptado);
-
-        //IMPRIMIR EN UNA VARIABLE EL ACUMULADO - LA PALABRA ENCRIPTADA.
-        resultadoEncriptado += stringEncriptado;
-      }else{
-        alert('SOLO INGRESA LETRAS');
+        if (codigoAscci >= 65 && codigoAscci <= 90) {
+          // FORMULA PARA ENCONTRAR LA NUEVA POSICIÓN EN EL CODIGO ASCCI.
+          const nuevaPosicion = ((codigoAscci - 65 + offset) % 26) + 65;
+          console.log(nuevaPosicion);
+          // CONVERTIR LA NUEVA POSICIÓN DEL CÓDIGO ASCCI EN LETRA.
+          const stringEncriptado = String.fromCharCode(nuevaPosicion);
+          console.log(stringEncriptado);
+          // IMPRIMIR EN UNA VARIABLE EL ACUMULADO - LA PALABRA ENCRIPTADA.
+          resultadoEncriptado += stringEncriptado;
+          // ** VALIDANDO LAS LETRAS MINUSCULAS A = 97 y Z = 122 **
+        } else if (codigoAscci >= 97 && codigoAscci <= 122) {
+          // FORMULA PARA ENCONTRAR LA NUEVA POSICIÓN EN EL CODIGO ASCCI.
+          const nuevaPosicion = ((codigoAscci - 97 + offset) % 26) + 97;
+          console.log(nuevaPosicion);
+          // CONVERTIR LA NUEVA POSICIÓN DEL CÓDIGO ASCCI EN LETRA.
+          const stringEncriptado = String.fromCharCode(nuevaPosicion);
+          console.log(stringEncriptado);
+          // IMPRIMIR EN UNA VARIABLE EL ACUMULADO - LA PALABRA ENCRIPTADA.
+          resultadoEncriptado += stringEncriptado;
+        } else {
+          alert('SOLO INGRESA LETRAS');
+          break;
+        }
+      } else {
+        alert('INGRESA UN NRO POSITIVO MAYOR A CERO !!');
         break;
       }
-    }else{
-      alert('INGRESA UN NRO POSITIVO MAYOR A CERO !!');
-      break;
-    }            
-  }
-  // IMPRIMIR RESULTADO EN CONSOLA.
-  console.log("result: ",resultadoEncriptado);
-  // RETORNAR ACUMULADO DEL MENSAJE.
-  return resultadoEncriptado;
+    }
+    // IMPRIMIR RESULTADO EN CONSOLA.
+    console.log(resultadoEncriptado);
+    // RETORNAR ACUMULADO DEL MENSAJE.
+    return resultadoEncriptado;
   },
 
-  decode : (offset, string) => {
-
+  decode: (offset, string) => {
     let resultadoDesencriptado = '';
 
-    for (let i = 0; i < string.length; i++) {
+    for (let i = 0; i < string.length; i += 1) {
     // ENGLOBAR "cada letra de la palabra string" EN UNA VARIABLE.
-    let palabra = string[i];
-    console.log(palabra);
+      const palabra = string[i];
+      console.log(palabra);
 
-    // CONVERTIR "cada letra de la palabra string" EN CODIGO ASCII.
-    let codigoAscci = string.charCodeAt(i);
-    console.log(codigoAscci);
+      // CONVERTIR "cada letra de la palabra string" EN CODIGO ASCII.
+      const codigoAscci = string.charCodeAt(i);
+      console.log(codigoAscci);
 
-    // ** VALIDANDO NUMERO POSITIVO **
-    if (offset > 0){
-    // ** VALIDANDO LAS LETRAS MAYUSCULA A = 65 y Z = 90 **
-
-      if(codigoAscci >= 65 && codigoAscci <= 90){
-      // FORMULA PARA ENCONTRAR LA NUEVA POSICIÓN EN EL CODIGO ASCCI.
-      let nuevaPosicion = ((codigoAscci - 65 - offset + 26 * 2 ) % 26 ) + 65;
-      console.log(nuevaPosicion);
-
-      // CONVERTIR LA NUEVA POSICIÓN DEL CÓDIGO ASCCI EN LETRA.
-      let stringEncriptado = String.fromCharCode(nuevaPosicion);
-      console.log(stringEncriptado);
-
-      //IMPRIMIR EN UNA VARIABLE EL ACUMULADO - LA PALABRA ENCRIPTADA.
-      resultadoDesencriptado += stringEncriptado;  
-      
-      // ** VALIDANDO LAS LETRAS MINUSCULAS A = 97 y Z = 122 **
-      }else if(codigoAscci >= 97 && codigoAscci <= 122){
-      // FORMULA PARA ENCONTRAR LA NUEVA POSICIÓN EN EL CODIGO ASCCI.
-      let nuevaPosicion = ((codigoAscci - 97 - offset + 26 * 2 ) % 26 ) + 97;
-      console.log(nuevaPosicion);
-
-      // CONVERTIR LA NUEVA POSICIÓN DEL CÓDIGO ASCCI EN LETRA.
-      let stringEncriptado = String.fromCharCode(nuevaPosicion);
-      console.log(stringEncriptado);
-
-      //IMPRIMIR EN UNA VARIABLE EL ACUMULADO - LA PALABRA ENCRIPTADA.
-      resultadoDesencriptado += stringEncriptado;  
-      
-      }else{
-        alert('SOLO INGRESA LETRAS');
+      // ** VALIDANDO NUMERO POSITIVO **
+      if (offset > 0) {
+      // ** VALIDANDO LAS LETRAS MAYUSCULA A = 65 y Z = 90 **
+        if (codigoAscci >= 65 && codigoAscci <= 90) {
+          // FORMULA PARA ENCONTRAR LA NUEVA POSICIÓN EN EL CODIGO ASCCI.
+          const nuevaPosicion = ((codigoAscci - 65 - offset + 26 * 2) % 26) + 65;
+          console.log(nuevaPosicion);
+          // CONVERTIR LA NUEVA POSICIÓN DEL CÓDIGO ASCCI EN LETRA.
+          const stringEncriptado = String.fromCharCode(nuevaPosicion);
+          console.log(stringEncriptado);
+          // IMPRIMIR EN UNA VARIABLE EL ACUMULADO - LA PALABRA ENCRIPTADA.
+          resultadoDesencriptado += stringEncriptado;
+          // ** VALIDANDO LAS LETRAS MINUSCULAS A = 97 y Z = 122 **
+        } else if (codigoAscci >= 97 && codigoAscci <= 122) {
+          // FORMULA PARA ENCONTRAR LA NUEVA POSICIÓN EN EL CODIGO ASCCI.
+          const nuevaPosicion = ((codigoAscci - 97 - offset + 26 * 2) % 26) + 97;
+          console.log(nuevaPosicion);
+          // CONVERTIR LA NUEVA POSICIÓN DEL CÓDIGO ASCCI EN LETRA.
+          const stringEncriptado = String.fromCharCode(nuevaPosicion);
+          console.log(stringEncriptado);
+          // IMPRIMIR EN UNA VARIABLE EL ACUMULADO - LA PALABRA ENCRIPTADA.
+          resultadoDesencriptado += stringEncriptado;
+        } else {
+          alert('SOLO INGRESA LETRAS');
+          break;
+        }
+      } else {
+      // eslint-disable-next-line no-alert
+        alert('INGRESA UN NRO POSITIVO MAYOR A CERO !!');
         break;
       }
-  
-    }else{
-      alert('INGRESA UN NRO POSITIVO MAYOR A CERO !!');
-      break;
     }
+    // IMPRIMIR RESULTADO EN CONSOLA.
+    console.log(resultadoDesencriptado);
 
-  }
-  // IMPRIMIR RESULTADO EN CONSOLA.
-  console.log("result",resultadoDesencriptado);
-
-  // RETORNAR ACUMULADO DEL MENSAJE.
-  return resultadoDesencriptado;
-
-  }
-
+    // RETORNAR ACUMULADO DEL MENSAJE.
+    return resultadoDesencriptado;
+  },
 };
--- src/index.js
+++ src/index.js
@@ -1,58 +1,58 @@
 // JALAR LOS ELEMENTOS DEL HTML - DOM
 
 // 1°Elementos para ENCRIPTAR
-let txtEncriptar = document.getElementById("txtEncriptar");
-let txtNroEncriptar = document.getElementById("txtNroEncriptar");
-let txtRespuesta1 = document.getElementById("txtRespuesta1");
-const btnEncriptar = document.getElementById("btnEncriptar");
+const txtEncriptar = document.getElementById('txtEncriptar');
+const txtNroEncriptar = document.getElementById('txtNroEncriptar');
+const txtRespuesta1 = document.getElementById('txtRespuesta1');
+const btnEncriptar = document.getElementById('btnEncriptar');
 
 // Evento Click para Encriptar.
-btnEncriptar.addEventListener("click", () => {
-    cadena = txtEncriptar.value;  // bien raro sin let o const funciona.
-    posicion = txtNroEncriptar.value;
-    if(cadena.length > 0 && posicion.length > 0){
-    txtRespuesta1.innerHTML = window.cipher.encode(parseInt(txtNroEncriptar.value), txtEncriptar.value);
-    }else{
-        alert("INGRESAR AMBOS CAMPOS");
-    }
+btnEncriptar.addEventListener('click', () => {
+  const cadena = txtEncriptar.value;
+  const posicion = txtNroEncriptar.value;
+  if (cadena.length > 0 && posicion.length > 0) {
+    txtRespuesta1.innerHTML = window.cipher.encode(parseInt(txtNroEncriptar.value, 10),
+      txtEncriptar.value);
+  } else {
+    alert('INGRESAR AMBOS CAMPOS');
+  }
 });
 
 // 2° Elementos para pasar a la primera vista.
-const btnSgte = document.getElementById("btnSgte");
-const inicio = document.getElementById("inicio");
-const primeraVista = document.getElementById("primeraVista");
+const btnSgte = document.getElementById('btnSgte');
+const inicio = document.getElementById('inicio');
+const primeraVista = document.getElementById('primeraVista');
 
 // Evento Click para pasar a la primera vista.
-btnSgte.addEventListener("click", () => {
-    cadena = txtEncriptar.value;  // bien raro sin let o const funciona.
-    posicion = txtNroEncriptar.value;
+btnSgte.addEventListener('click', () => {
+  const cadena = txtEncriptar.value;
+  const posicion = txtNroEncriptar.value;
+  // resultado = txtRespuesta1.value; *SE TIENE QUE VALIDAR EL RESULTADO*
 
-    //resultado = txtRespuesta1.value; ***************SE TIENE QUE VALIDAR EL RESULTADO********************
-
-    if(cadena.length > 0 && posicion.length > 0){
-    inicio.classList.add("hide"); // Ocultas
+  if (cadena.length > 0 && posicion.length > 0) {
+    inicio.classList.add('hide'); // Ocultas
     primeraVista.classList.remove('hide'); // Muestras.
-    }else{
-        alert("TIENE QUE ENCRIPTAR");
-    }
-    
+  } else {
+    alert('TIENE QUE ENCRIPTAR');
+  }
 });
 
 // 3° Elementos para DESENCRIPTAR.
 
-let txtDesencriptar = document.getElementById('txtDesencriptar');
-let txtNroDesencriptar = document.getElementById('txtNroDesencriptar');
-let txtRespuesta2 = document.getElementById("txtRespuesta2");
+const txtDesencriptar = document.getElementById('txtDesencriptar');
+const txtNroDesencriptar = document.getElementById('txtNroDesencriptar');
+const txtRespuesta2 = document.getElementById('txtRespuesta2');
 const btnDesencriptar = document.getElementById('btnDesencriptar');
 
 btnDesencriptar.addEventListener('click', () => {
-    cadena = txtDesencriptar.value;
-    posicion = txtNroDesencriptar.value;
-    if(cadena.length > 0 && posicion.length > 0){
-    txtRespuesta2.innerHTML = window.cipher.decode(parseInt(txtNroDesencriptar.value), txtDesencriptar.value);
-    }else{
-        alert("INGRESAR AMBOS CAMPOS");
-    }
+  const cadena = txtDesencriptar.value;
+  const posicion = txtNroDesencriptar.value;
+  if (cadena.length > 0 && posicion.length > 0) {
+    txtRespuesta2.innerHTML = window.cipher.decode(parseInt(txtNroDesencriptar.value, 10),
+      txtDesencriptar.value);
+  } else {
+    alert('INGRESAR AMBOS CAMPOS');
+  }
 });
 
 // 4° Elementos para pasar a la segunda vista.
@@ -61,15 +61,13 @@ const segundaVista = document.getElementById('segundaVista');
 
 // Evento Click para pasar a la segunda vista.
 btnFin.addEventListener('click', () => {
-
-    cadena = txtDesencriptar.value;
-    posicion = txtNroDesencriptar.value;
-
-    //resultado = txtRespuesta1.value; ***************SE TIENE QUE VALIDAR EL RESULTADO********************
-    if(cadena.length > 0 && posicion.length > 0){
+  const cadena = txtDesencriptar.value;
+  const posicion = txtNroDesencriptar.value;
+  // resultado = txtRespuesta1.value; *SE TIENE QUE VALIDAR EL RESULTADO*
+  if (cadena.length > 0 && posicion.length > 0) {
     primeraVista.classList.add('hide'); // Ocultas
     segundaVista.classList.remove('hide'); // Muestras
-    }else{
-        alert("TIENE QUE DESENCRIPTAR"); 
-    }
+  } else {
+    alert('TIENE QUE DESENCRIPTAR');
+  }
 });
